b878726f61ddd1497bb54cab61b86410468e7a23
User <user@192.168.1.9>
2018-05-19 16:29:42 +0700

    contact v1.1

--- app/src/main/java/com/a4nesia/whatsappclone/ContactActivity.java
+++ app/src/main/java/com/a4nesia/whatsappclone/ContactActivity.java
@@ -1,17 +1,46 @@
 package com.a4nesia.whatsappclone;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ContactActivity extends AppCompatActivity {
-
+    AlertDialog addContactDialog;
+    EditText editSearch;
+    CircleImageView imgSearch;
+    TextView textSearch;
+    RelativeLayout layoutSearch;
+    ProgressBar loading;
+    Button btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        View alertView = getLayoutInflater().inflate(R.layout.dialog_add_contact,null);
+        editSearch = (EditText)alertView.findViewById(R.id.search_edit);
+        imgSearch = (CircleImageView)alertView.findViewById(R.id.search_img);
+        textSearch = (TextView)alertView.findViewById(R.id.search_nama);
+        loading = (ProgressBar)alertView.findViewById(R.id.loading);
+        layoutSearch = (RelativeLayout)alertView.findViewById(R.id.search_layout);
+        btnSearch = (Button)alertView.findViewById(R.id.search_add);
+        addContactDialog = new AlertDialog.Builder(this)
+                .setTitle("Add New Contact")
+                .setView(alertView)
+                .create();
+
+
     }
 
     @Override
@@ -24,7 +53,9 @@ public class ContactActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.add_contact){
-
+            editSearch.setText("");
+            layoutSearch.setVisibility(View.GONE);
+            addContactDialog.show();
         }
         else if(id==android.R.id.home){
             super.onBackPressed();
