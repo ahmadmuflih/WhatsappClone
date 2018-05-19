664d9e6a9b85ee529cda6329e3ec37f7607c67b0
User <user@192.168.1.9>
2018-05-19 15:58:54 +0700

    contact v1.0

---
+++ app/src/main/java/com/a4nesia/whatsappclone/ContactActivity.java
@@ -0,0 +1,34 @@
+package com.a4nesia.whatsappclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_contact,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.add_contact){
+
+        }
+        else if(id==android.R.id.home){
+            super.onBackPressed();
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/a4nesia/whatsappclone/MainActivity.java
+++ app/src/main/java/com/a4nesia/whatsappclone/MainActivity.java
@@ -1,8 +1,15 @@
 package com.a4nesia.whatsappclone;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +27,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     TextView textNama, textStatus, textNomor;
     CircleImageView imgFoto;
+    AlertDialog editNameDialog;
     String TAG = "WA";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,5 +108,53 @@ public class MainActivity extends AppCompatActivity {
 //                Log.e(TAG,t.getMessage());
 //            }
 //        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.show_contact){
+            startActivity(new Intent(getApplicationContext(), ContactActivity.class));
+        }
+        else if(id==R.id.logout){
+            Preferences.setBooleanPreference("login",false,getApplicationContext());
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            finish();
+        }
+        return true;
+    }
+    public void editName(View view){
+        View alertView = getLayoutInflater().inflate(R.layout.dialog_edit_name,null);
+        final EditText editNama = (EditText)alertView.findViewById(R.id.edit_name);
+        String nama = Preferences.getStringPreference("nama",getApplicationContext());
+        editNama.setText(nama);
+        editNameDialog = new AlertDialog.Builder(this)
+                .setTitle("Edit Name")
+                .setView(alertView)
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String newName = editNama.getText().toString().trim();
+                        if(newName.equals("")){
+                            Toast.makeText(MainActivity.this, "Nama tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Preferences.setStringPreference("nama", newName, getApplicationContext());
+                            textNama.setText(newName);
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).create();
+        editNameDialog.show();
     }
 }
